a62af3080b84bb81f70c4fd651758195c508e877
quangnvo <voquang.usth@gmail.com>
2023-05-31 22:10:02 +0800

    modify padding of website

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import { useMDXComponent } from "next-contentlayer/hooks"
 
-
 const mdxComponents = {
     Image,
     TheCuaQuang: () => <div className="px-3 py-2 border border-black rounded-lg shadow-lg my-5">The cua Quang</div>,
@@ -13,6 +12,5 @@ interface MdxProps {
 
 export function Mdx({ code }: MdxProps) {
     const Component = useMDXComponent(code)
-
     return <Component components={mdxComponents} />
 }
